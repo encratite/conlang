3f0ad0b20c36a7939221660b734b5d217982ee33
void <git.email.address@googlemail.com>
2011-08-13 18:04:09 +0200

    Painful

--- application/Generator.rb
+++ application/Generator.rb
@@ -9,19 +9,24 @@ module Generator
   Vowels = [
     'a',
     'i',
-    'o',
+    'u',
     'e',
+    'o',
   ]
 
   InitialConsonants = [
-    'k_w',
-
     'p',
     't',
     'k',
 
+    'g',
+    'b',
+    'd',
+
     'j',
     'w',
+
+    'h',
 
     '?',
   ]
@@ -32,106 +37,30 @@ module Generator
 
     'l',
 
-    'tS',
-    'ts',
-    'tK',
-
+    'f',
     's',
     'S',
+
+    'tS',
+    'ts',
   ]
 
   Consonants = ExtendingConsonants + InitialConsonants
-  Neutral = ['']
 
+  Initials = InitialConsonants * Vowels - ['ji', 'wu']
+
+  Words = [
+    Initials,
+    Initials * ExtendingConsonants,
+    Initials * ExtendingConsonants * Vowels,
+    Initials * ExtendingConsonants * Vowels * ExtendingConsonants,
+  ]
   SyllableCounts = [
     1,
     1,
     2,
     2,
   ]
-
-  def self.loadLexicon
-    consonantVowelFrequencies, finalConsonantFrequencies, consonantPairs = Nahuatl.loadLexiconData
-    initialConsonantFrequencies = {}
-    extendingConsonantFrequencies = {}
-    consonantVowelFrequencies.each do |segments, frequency|
-      consonant, vowel = segments
-      if InitialConsonants.include?(consonant)
-        initialConsonantFrequencies[segments] = frequency
-      end
-      if ExtendingConsonants.include?(consonant)
-        extendingConsonantFrequencies[segments] = frequency
-      end
-    end
-    initialSyllables = []
-    initialSyllablesScale = Nil::RandomScale.new
-    initialConsonantFrequencies.each do |segments, frequency|
-      consonant, vowel = segments
-      string = consonant + vowel
-      initialSyllablesScale.add(string, frequency)
-      initialSyllables << segments
-    end
-    extendingSyllables = []
-    extendingSyllablesScale = Nil::RandomScale.new
-    extendingConsonantFrequencies.each do |segments, frequency|
-      consonant, vowel = segments
-      string = consonant + vowel
-      extendingSyllablesScale.add(string, frequency)
-      extendingSyllables << segments
-    end
-    finalConsonants = []
-    finalConsonantsScale = Nil::RandomScale.new
-    finalConsonantFrequencies.each do |consonant, frequency|
-      finalConsonantsScale.add(consonant, frequency)
-      finalConsonants << consonant
-    end
-    simpleMonosyllables = initialSyllables.map { |a, b| a + b } * Neutral
-    complexMonosyllables = []
-    initialSyllables.each do |initialSyllable|
-      initialConsonant, initialVowel = initialSyllable
-      finalConsonants.each do |finalConsonant|
-        pair = initialConsonant, finalConsonant
-        next if initialConsonant != '?' && !consonantPairs.include?(pair)
-        string = initialConsonant + initialVowel + finalConsonant
-        complexMonosyllables << string
-      end
-    end
-    simpleBisyllables = []
-    initialSyllables.each do |initialSyllable|
-      initialConsonant, initialVowel = initialSyllable
-      extendingSyllables.each do |extendingSyllable|
-        extendingConsonant, extendingVowel = extendingSyllable
-        pair = initialConsonant, extendingConsonant
-        next if initialConsonant != '?' && !consonantPairs.include?(pair)
-        string = initialConsonant + initialVowel + extendingConsonant + extendingVowel
-        simpleBisyllables << string
-      end
-    end
-    complexBisyllables = []
-    initialSyllables.each do |initialSyllable|
-      initialConsonant, initialVowel = initialSyllable
-      extendingSyllables.each do |extendingSyllable|
-        extendingConsonant, extendingVowel = extendingSyllable
-        pair = initialConsonant, extendingConsonant
-        next if initialConsonant != '?' && !consonantPairs.include?(pair)
-        finalConsonants.each do |finalConsonant|
-          pair = extendingConsonant, finalConsonant
-          next if !consonantPairs.include?(pair)
-          string = initialConsonant + initialVowel + extendingConsonant + extendingVowel + finalConsonant
-          complexBisyllables << string
-        end
-      end
-    end
-    words = [
-      simpleMonosyllables,
-      complexMonosyllables,
-      simpleBisyllables,
-      complexBisyllables,
-    ]
-    syllables = initialSyllables, extendingSyllables, finalConsonants
-    scales = [initialSyllablesScale, extendingSyllablesScale, finalConsonantsScale]
-    return syllables, scales, words
-  end
 
   def self.totalWordCount
     count = 0
@@ -153,19 +82,8 @@ module Generator
   end
 
   def self.generateWord(priority)
-    output = InitialSyllableScale.get
-    case priority
-    when 0
-    when 1
-      output += FinalConsonantsScale.get
-    when 2
-      output += ExtendingSyllablesScale.get
-    when 3
-      output += ExtendingSyllablesScale.get + FinalConsonantsScale.get
-    else
-      raise "Invalid priority: #{priority}"
-    end
-    return output
+    wordClass = Words[priority]
+    return wordClass[rand(wordClass.size)]
   end
 
   def self.describe
@@ -209,7 +127,7 @@ module Generator
     attempts = 100
     attempts.times do
       word = self.generateWord(priority)
-      next if usedWords.include?(word) || !Words[priority].include?(word)
+      next if usedWords.include?(word)
       return word
     end
     unusedWords = Generator::Words[priority].reject do |word|
@@ -228,25 +146,4 @@ module Generator
     end
     return output.sort
   end
-
-  def self.printSyllables
-    separator = ', '
-    puts "Initial syllables (#{InitialSyllables.size}):"
-    puts self.getSyllableStrings(InitialSyllables).join(separator)
-    puts "Extending syllables (#{ExtendingSyllables.size}):"
-    puts self.getSyllableStrings(ExtendingSyllables).join(separator)
-    puts "Final consonants (#{FinalConsonants.size}):"
-    puts FinalConsonants.sort.join(separator)
-  end
-
-  LexiconData = self.loadLexicon
-  Syllables = LexiconData[0]
-  InitialSyllables = Syllables[0]
-  ExtendingSyllables = Syllables[1]
-  FinalConsonants = Syllables[2]
-  Scales = LexiconData[1]
-  InitialSyllableScale = Scales[0]
-  ExtendingSyllablesScale = Scales[1]
-  FinalConsonantsScale = Scales[2]
-  Words = LexiconData[2]
 end
